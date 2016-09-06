e779b9f1b0ee712944925c5e2a5c5ed7c1b02a00
heray1990 <heray1990@hotmail.com>
2016-09-06 15:10:55 +0800

    Merge the solution from UnitTest/UnitTest009_PalindromeNumber.c to Solution/009_PalindromeNumber.c. Note: This version of solution is accepted by leetcode.com.

--- Solution/009_PalindromeNumber.c
+++ Solution/009_PalindromeNumber.c
@@ -1,10 +1,5 @@
-typedef enum {
-  false,
-  true
-} bool;
-
 bool isPalindrome(int x) {
-  int digitCnt = 1;
+  int digitCnt = 0;
   int n, i, k, resPower, remainder, quotient;
 
   if (x < 0)
@@ -13,11 +8,11 @@ bool isPalindrome(int x) {
   if (x >= 0 && x < 10)
     return true;
 
-  resPower = 10;
+  k = x;
   do {
     digitCnt++;
-    resPower *= 10; 
-  } while (x / resPower != 0);
+    k = k / 10; 
+  } while (k != 0);
   printf("The digit number of %d is %d.\n", x, digitCnt);
 
   k = digitCnt;
