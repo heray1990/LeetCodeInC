b1a8a8d3a617d0b95997ecd1966abd1281bd283b
heray1990 <heray1990@hotmail.com>
2017-06-07 17:17:52 +0800

    [#2 Add Two Numbers] Add 002_AddTwoNumbers.c file for the solution.

---
+++ Solution/002_AddTwoNumbers.c
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+	int tmp = 0;
+
+	struct ListNode* retls = NULL;
+	retls = malloc(sizeof(struct ListNode));
+	if(retls == NULL) {
+		printf("malloc failed!\n");
+		return retls;
+	}
+	retls->val = 0;
+	retls->next = NULL;
+
+	struct ListNode *curl1 = l1;
+	struct ListNode *curl2 = l2;
+	struct ListNode *curret = retls;
+
+	while((curl1 != NULL) && (curl2 != NULL)) {
+		tmp = curret->val + curl1->val + curl2->val;
+		curret->val = tmp % 10;
+		// Adding an new item to the end of the retls.
+		curret->next = malloc(sizeof(struct ListNode));
+		curret->next->val = tmp / 10;
+		curret->next->next = NULL;
+
+		curl1 = curl1->next;
+		curl2 = curl2->next;
+		curret = curret->next;
+	}
+
+	while(curl1 != NULL) {
+		tmp = curret->val + curl1->val;
+		curret->val = tmp % 10;
+
+		if((curl1->next == NULL) && (tmp < 10)) {
+			curret->next = NULL;
+		}
+		else {
+			curret->next = malloc(sizeof(struct ListNode));
+			curret->next->val = tmp / 10;
+			curret->next->next = NULL;
+			curret = curret->next;
+		}
+
+		curl1 = curl1->next;
+	}
+
+	while(curl2 != NULL) {
+		tmp = curret->val + curl2->val;
+		curret->val = tmp % 10;
+
+		if((curl2->next == NULL) && (tmp < 10)) {
+			curret->next = NULL;
+		}
+		else {
+			curret->next = malloc(sizeof(struct ListNode));
+			curret->next->val = tmp / 10;
+			curret->next->next = NULL;
+			curret = curret->next;
+		}
+
+		curl2 = curl2->next;
+	}
+
+	return retls;
+}
