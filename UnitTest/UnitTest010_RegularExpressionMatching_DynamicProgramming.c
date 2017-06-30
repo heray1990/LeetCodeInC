85df15a329f3dcf22973651628744d0cca6f5004
heray1990 <heray1990@hotmail.com>
2017-06-30 16:22:34 +0800

    [#10 Regular Expression Matching] A solution for the problem using Dynamic Programming.

---
+++ UnitTest/UnitTest010_RegularExpressionMatching_DynamicProgramming.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+	false,
+	true
+} bool;
+
+bool isMatch(const char *s, const char *p) {
+	int i = 0, j = 0;
+	int sLen = strlen(s);
+	int pLen = strlen(p);
+	bool res = false;
+	bool **table = NULL;
+
+	if(sLen == 0 && pLen == 0)
+		return true;
+
+	if(pLen == 0)
+		return false;
+
+	// Initialize table with false.
+	table = (bool **)malloc((pLen + 1) * sizeof(bool*));
+	for(j = 0; j <= pLen; j++) {
+		*(table + j) = (bool *)malloc((sLen + 1) * sizeof(bool));
+	}
+	for(j = 0; j <= pLen; j++) {
+		for(i = 0; i <= sLen; i++) {
+			table[j][i] = false;
+		}
+	}
+
+	table[0][0] = true;
+
+	for(j = 0; j < pLen; j++) {
+		if(p[j] != '*') {
+			for(i = 0; i < sLen; i++) {
+				if(s[i] == p[j] || p[j] == '.') {
+					table[j + 1][i + 1] = table[j][i];
+				}
+			}
+		}
+		else {
+			if(j > 0) {
+				if(p[j - 1] == '.') {
+					i = 0;
+					while(i < sLen && !table[j - 1][i + 1] 
+						&& !table[j][i + 1]) {
+						i++;
+					}
+
+					for(; i < sLen; i++) {
+						table[j + 1][i + 1] = true;
+					}
+				}
+				else {
+					for(i = 0; i < sLen; i++) {
+						if(table[j - 1][i + 1] || table[j][i + 1]
+							|| i > 0 && j > 0 && table[j + 1][i]
+							&& s[i] == s[i - 1] && s[i - 1] == p[j - 1]) {
+							table[j + 1][i + 1] = true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	res = table[pLen][sLen];
+
+	for(i = 0; i < sLen; i++) {
+		free(*(table + i));
+		*(table + i) = NULL;
+	}
+	free(table);
+	table = NULL;
+
+	return res;
+}
+
+int main() {
+	const char *s = "abcdddg";
+	const char *p = "a.*d*g";
+
+	printf("Input:\"%s\", \"%s\"\n", s, p);
+	if(isMatch(s, p) == 1) {
+		printf("Output: True\n");
+	}
+	else {
+		printf("Output: False\n");
+	}
+
+	return 1;
+}
